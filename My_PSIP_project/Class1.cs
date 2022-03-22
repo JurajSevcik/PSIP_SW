40a1b5cdc8c68a2936b59c946399060a6dd25318
JurajSevcik <71790193+JurajSevcik@users.noreply.github.com>
2022-03-22 11:14:43 +0100

    upravy mac table ...skoro hotovo , este jej chyba zobrazovanie

--- My_PSIP_project/Class1.cs
+++ My_PSIP_project/Class1.cs
@@ -12,10 +12,45 @@ namespace My_PSIP_project
     internal class PacketSender
     {
         Libraly L = new Libraly();
+        table_class T = new table_class();
         
-        public void send()
+        public void send(SharpPcap.RawCapture rawPacket, char port)  //get packet and where it come from
         {
-            Console.WriteLine(L.device_a);
+            var packet = PacketDotNet.Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+            var ethernetPacket = (EthernetPacket)packet;
+            string ToMac = (ethernetPacket.DestinationHardwareAddress).ToString();
+            //todo: chack rouls for sending packets 
+            //Console.WriteLine(L.device_a);
+            char intf = T.WhereDoIGO(ToMac);
+            LibPcapLiveDevice gate = L.device_a;
+            //send packet to other port 
+            if (intf == 'X') //no idead wher to go --> everywhere but home 
+            {
+                if (port == 'A') { gate = L.device_b; } else { gate = L.device_a; }// other port 
+                go(gate, rawPacket);
+            }
+            else if(intf == 'A') { 
+                gate = L.device_a;
+                go(gate, rawPacket);
+            }
+            else if (intf == 'B')
+            {
+                gate = L.device_b;
+                go(gate, rawPacket);
+            }
+
+        }
+
+
+        private void go(LibPcapLiveDevice gate, SharpPcap.RawCapture rawPacket)
+        {
+            gate.Open(mode: DeviceModes.Promiscuous | DeviceModes.DataTransferUdp | DeviceModes.NoCaptureLocal, read_timeout: 1000);   //open send packet close
+            try
+            {
+                gate.SendPacket(rawPacket.Data);
+            }
+            catch (Exception) { Console.WriteLine("-- error"); }
+            gate.Close();
         }
     }
 }
